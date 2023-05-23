22455d4631712dc0866983757ce5aad9aa9568bc
codynego <emonenaabednego@gmail.com>
2023-05-23 11:34:07 +0100

    get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,6 +6,7 @@ A flask application
 from flask_babel import Babel
 from flask import Flask
 from flask import render_template
+from flask import request
 
 
 class Config(object):
@@ -23,6 +24,14 @@ app.config.from_object(Config)
 
 
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
