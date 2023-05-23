b54c635f35be8ae8e5fdde5d29fe1ffc5349189d
codynego <emonenaabednego@gmail.com>
2023-05-23 12:09:19 +0100

    translation

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+A flask application
+"""
+from flask_babel import Babel
+from flask import Flask
+from flask import render_template
+from flask import request
+
+
+class Config(object):
+    """
+    configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the applicatioN
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    render an html template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
